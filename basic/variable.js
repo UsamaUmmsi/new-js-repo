d2c918372c3784167400ad9831f6c7cea8eaa78a
Ummsi <usamakhan@digitalgravity.ae>
2025-07-04 11:53:37 +0000

    container config done

---
+++ basic/variable.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountEmail = "ummsi@gmail.com";
+var accountPassword = "212121";
+accountCity = "karachi"
+
+// accountId = 2 not allowed
+accountEmail = "expample@gmail.com"
+accountPassword = "99876542"
+console.log(accountId);
+console.log("");
+
