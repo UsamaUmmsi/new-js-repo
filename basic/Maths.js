150b350086e91f434509f2cfccb57bd814b38432
Ummsi <usamakhan@digitalgravity.ae>
2025-07-22 13:33:53 +0000

    container config done

---
+++ basic/Maths.js
@@ -0,0 +1,33 @@
+//*************Numbers******************
+
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 1123.8699
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString('en-pk'));
+
+
+// ****************Maths************** 
+
+
+console.log(Math.abs(4.5));
+console.log(Math.round(60.5));
+console.log(Math.floor(60.8));
+console.log(Math.random());
+console.log(Math.random()*10) +1;
+
+
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random()*(max- min + 1)));
+
