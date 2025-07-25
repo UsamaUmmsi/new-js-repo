1a2af55ecbb3b177db7cd8f5453a928c81cf9e5f
Ummsi <usamakhan@digitalgravity.ae>
2025-07-25 11:49:27 +0000

    Arrays config

---
+++ array/array.js
@@ -0,0 +1,46 @@
+// const myArray  = ["usama", "khan", "dg"]
+
+// // console.log(myArray[2]);
+
+const myArray2 = new Array(1,2,3,4,5,6,7,8,9,10)
+// console.log(myArray2[2]);
+
+// Shallow Copy
+// Shallow copy ka matlab hai ke object ki sirf upar wali (top-level) properties copy hoti hain. Agar koi property
+// object ya array ho (nested ho), to reference hi copy hota hai, actual data nahi.
+
+
+
+// Deep Copy
+
+
+// array push[]
+// .push() use hota hai add karne k liye 
+// .pop() use hota akhri value remove karne k liye  
+
+// myArray2.push(11)
+// myArray2.pop(12)
+// myArray2.push(12)
+
+// // myArray2.unshift(15)
+// myArray2.shift()
+
+
+// console.log(myArray2);
+
+// myArray2.includes()
+// const newArry = myArray2.join() 
+
+// console.log(myArray2.includes(5));
+// console.log(myArray2.indexOf(5));
+// console.log(newArry);
+
+
+// slice , splice
+
+
+const myAar = myArray2.slice( 1 , 3)
+
+console.log(myAar);
+console.log("B", myArray2);
+
